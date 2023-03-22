b3491eb75fbde664e6e5cc59fff0e0d83ee236a3
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-22 17:48:02 +0500

    16. Spring Data JPA with Spring MVC - Testing for expected Exceptions

---
+++ src/test/java/springFrameworkSpringBoot/S9SpringDataJPAWithSpringMVC/controllers/BeerControllerS9TestIT.java
@@ -0,0 +1,59 @@
+package springFrameworkSpringBoot.S9SpringDataJPAWithSpringMVC.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import springFrameworkSpringBoot.S8ExceptionHandling.NotFoundException;
+import springFrameworkSpringBoot.S9SpringDataJPAWithSpringMVC.Entities.BeerEntity;
+import springFrameworkSpringBoot.S9SpringDataJPAWithSpringMVC.Model.BeerDTO;
+import springFrameworkSpringBoot.S9SpringDataJPAWithSpringMVC.repositories.BeerRepositoryS9;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerS9TestIT {
+    @Autowired
+    BeerControllerS9 beerController;
+
+    @Autowired
+    BeerRepositoryS9 beerRepository;
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        BeerEntity beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
